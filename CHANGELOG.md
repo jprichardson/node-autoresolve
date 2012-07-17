7f6248bd14da77cee0bbe4db51a1504772b0abb5
JP Richardson <jprichardson@gmail.com>
2012-07-17 12:50:13 -0500

    Bug fix on absolute paths.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+0.0.2 / 2012-07-17
+------------------
+* Bug fix: error if an absolute directory was passed in.
+
 0.0.1 / 2012-07-17
 ------------------
 * Inital release.
--- test/autoresolve.test.js
+++ test/autoresolve.test.js
@@ -1,11 +1,27 @@
 var assert = require('assert'),
-    P = require('../lib/autoresolve');
+    P = require('../lib/autoresolve'),
+    fs = require('fs');
 
 var T = function(expr) { assert(expr); };
 
 describe('+ P()', function(){
-    it("should return the proper path.", function(){
+    it("should return the proper relative path.", function(){
         var customer = require(P('lib/models/customer'));
         T(customer.getName() === 'JP');
     });
+
+    it ("should return the absolute path.", function(done){
+        var path = P('lib/models/customer.js');
+        var fdr = fs.createReadStream(path);
+        var fdw = fs.createWriteStream('/tmp/customer.js');
+        
+        fdr.on('end', function(){
+            var customer = require(P('/tmp/customer'));
+            fs.unlinkSync('/tmp/customer.js');
+            T(customer.getName() === 'JP');
+            done();
+        });
+
+        fdr.pipe(fdw);
+    })
 });
